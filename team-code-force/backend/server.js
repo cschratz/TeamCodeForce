fb287fbc82d0739ac565394de205a872e10c16d9
Jay Steigner <ioctosteigner@gmail.com>
2020-08-07 16:16:10 -0500

    made schema, made server, installed dependencies gi# with '#' will be ignored, and an empty message aborts the commit.

---
+++ team-code-force/backend/server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+require('../db/index.js');
+
+const { SERVER_PORT } = process.env || 8080;
+
+// app.get('/', (req, res) => {
+//   console.log('get sent');
+//   res.send('getttt');
+// });
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is listening on ${SERVER_PORT}`);
+});
